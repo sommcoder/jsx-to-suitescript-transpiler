193dfe04212e4080504500cb7791d8b965c01a3e
Brian Davies <brian.davies589@gmail.com>
2023-02-26 20:49:24 -0500

    made some good changes, starting to use the actual node development environment now that I've tested what I can in AST Explorer

--- ast-explorer-working-file.js
+++ ast-explorer-working-file.js
@@ -12,8 +12,63 @@ function createPlugin(babel) {
     },
   };
 
-  // this is where we track the components and their insertion order
-  const page = new Map();
+  // this is where we track the components insertion order & their children
+  const pageArr = new Array();
+
+  function handleAttributes(attrsArr, path) {
+    const attributes = new Object();
+    attrsArr.forEach((attr) => {
+      // no duplicate attribute names!
+      if (attributes.hasOwnProperty(attr.name.name)) {
+        throw path.buildCodeFrameError(
+          `ERR: there is already an attribute called: ${attr.name.name} in the component: ${component}`
+        );
+      }
+
+      // handle attributes with null value (ie. disable/mandatory/selected)
+      console.log("attr.value:", attr.value);
+      if (attr.value == null) {
+        attributes[attr.name.name] = true;
+        return;
+      }
+
+      // handle string literals:
+      if (t.isStringLiteral(attr.value)) {
+        attributes[attr.name.name] = attr.value.value;
+        return;
+      }
+
+      // handle numeric literals:
+      if (
+        t.isJSXExpressionContainer(attr.value) &&
+        t.isNumericLiteral(attr.value.expression)
+      ) {
+        attributes[attr.name.name] = attr.value.expression.value;
+        return;
+      }
+
+      // handle attributes that have a binding IN FUNCTION SCOPE
+      if (
+        t.isJSXExpressionContainer(attr.value) &&
+        t.isIdentifier(attr.value.expression)
+      ) {
+        const identifiersObj = path.scope.bindings;
+        console.log(Object.keys(identifiersObj).length === 0);
+
+        // inside scope?
+        if (!Object.keys(identifiersObj).length === 0) {
+          let bindingIdNode =
+            identifiersObj[attr.value.expression.name].path.node;
+          attributes[attr.name.name] = bindingIdNode.init.value;
+        } else {
+          // outside scope! HOW DON WE GET THIS!!???
+        }
+
+        return;
+      }
+    });
+    return attributes;
+  }
 
   function createSSComponent(el, attr, children) {}
 
@@ -29,77 +84,19 @@ function createPlugin(babel) {
         console.log("JSX Element path:", path);
         const component = path.node.openingElement.name.name;
         console.log("component:", component);
+
+        // CHILDREN HANDLING********************
         const childArr = path.node.children;
-
-        // filter out children that are JSXText (ie. whitespace)
         const children = childArr.filter((el) => el.type !== "JSXText");
-        console.log("children:", children);
-
-        // get attributes array
+        // ATTRIBUTE HANDLING*******************
         const attrsArr = path.node.openingElement.attributes;
-        console.log("attribute arr:", attrsArr);
+        const attributes = handleAttributes(attrsArr, path);
 
-        let attributes = new Map();
-
-        attrsArr.forEach((attr) => {
-          console.log("attribute pairing:", attr.name.name, ":", attr.value);
-          console.log("attr:", attr);
-
-          // handle attributes with no value / binding (ie. disable/mandatory/selected)
-          console.log("attr.value:", attr.value);
-          if (attr.value == null) {
-            attributes.set(attr.name.name, attr.value.value);
-            // attributes[attr.name.name] = true;
-            return; // return instead of continue becasue we're in a forEach function
-            // and NOT a FOR loop!
-          }
-
-          // handle string literals
-          if (t.isStringLiteral(attr.value)) {
-            console.log("attr is a string literal");
-            // attr.value.value
-            // attr.name.name
-            attributes.set(attr.name.name, attr.value.value);
-            //attributes[attr.name.name] = attr.value.value;
-            console.log("attributesObj:", attributes);
-          }
-
-          // handle numeric literals:
-          if (
-            t.isJSXExpressionContainer(attr.value) &&
-            t.isNumericLiteral(attr.value.expression)
-          ) {
-            console.log("numeric literal");
-            console.log(attr.name.name, "", attr.value.expression.value);
-            attributes[attr.name.name] = attr.value.expression.value;
-            return;
-          }
-
-          // handle attributes that have a binding IN FUNCTION SCOPE
-          if (
-            t.isJSXExpressionContainer(attr.value) &&
-            t.isIdentifier(attr.value.expression)
-          ) {
-            console.log("attr is a JSX expression container");
-            const identifiersObj = path.scope.bindings;
-            console.log("id obj", identifiersObj);
-            //console.log(attr.name.name); ---> this is the name of the JSXAttribute!
-            // we need the name of the JSXExpression --> attr.value.expression.name
-            let bindingIdNode =
-              identifiersObj[attr.value.expression.name].path.node;
-            console.log(bindingIdNode.id.name, ":", bindingIdNode.init.value);
-            // .path.node.id.name
-            // .path.node.init.value
-            attributes[attr.name.name] = bindingIdNode.init.value;
-            console.log("attributesObj:", attributes);
-            return;
-          }
-
-          // handle attributes that have a StringLiteral value
+        pageArr.push({
+          [component]: { attributes: attributes, children: children },
         });
 
-        page.set(component + "s", { [component]: attributes });
-        console.log("PAGE:", page);
+        console.log("PAGE:", pageArr);
       },
     },
   };
