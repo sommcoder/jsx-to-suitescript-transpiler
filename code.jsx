b55c10aae71708718aa4d6263c572f4279676276
Brian Davies <brian.davies589@gmail.com>
2023-03-07 18:24:03 -0500

    minor updates, needed to add some props to the library that I didnt include before

--- code.jsx
+++ code.jsx
@@ -2,16 +2,16 @@ function Page() {
   const fileId = "654321";
   const client = "123456";
   return (
-    <Form fileId={fileId} module={client}>
+    <Form title="Distribution Form" fileId={fileId} module={client}>
       <Sublist label="Item History" type="EDITOR">
         <Field label="Transaction id" type="number" def={123} />
         <Field label="Rate" type="text" />
         <Field label="Date" type="text" />
         <Field label="Entity" type="select">
-          <Select value="" text="" isSelected />
-          <Select value="" text="" />
-          <Select value="" text="" />
-          <Select value="" text="" />
+          <Select value="123" text="Google" isSelected />
+          <Select value="123" text="Facebook" />
+          <Select value="123" text="Netflix" />
+          <Select value="123" text="Airbnb" />
         </Field>
         <Button label="Reset Button" />
       </Sublist>
