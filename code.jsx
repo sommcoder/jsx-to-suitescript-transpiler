011920af91bfb71b8f147ccc4fbf4bcf00da3939
Brian Davies <brian.davies589@gmail.com>
2023-06-03 23:40:07 -0400

    Got the search.create() function working/looking correct. Just need to get the traversal of the ResultSet working and some final tests... hopefully!

--- code.jsx
+++ code.jsx
@@ -10,13 +10,20 @@ const settings = [
   },
 ];
 
+/*
+ 
+1) wrap which components you want to apply a search to
+2) add a filter and settings prop to Search if needed
+ 
+*/
+// PLOTTING JUST FIELDS:
 <Form title="Customer Form" fileId="654321">
   <Tab label="Customer Information">
     <Sublist markAll label="item history">
-      <Search settings={settings} filters={filters}>
+      <Search settings={settings} filters={filters} type="sales">
         <Field label="Entity" type="text" column="name" />
         <Field label="Subsidiary" type="text" column="subsidiary" />
-        <Field label="Name" type="text" column="name">
+        <Field label="Name" type="text" column="location">
           <Select text="Sampson" value={2} />
           <Select text="Albert" value={1} />
           <Select text="Geoff" value={3} />
@@ -29,6 +36,24 @@ const settings = [
   <Button label="Clear Button" />
 </Form>;
 
-context.response.writePage({
-    pageObject: ''
-});
+// PLOTTING SELECT FIELDS:
+{
+  /* <Form title="Customer Form" fileId="654321">
+  <Tab label="Customer Information">
+    <Sublist markAll label="item history">
+      <Field label="Entity" type="text" />
+      <Field label="Subsidiary" type="text" />
+      <Field label="Name" type="text">
+        <Search settings={settings} filters={filters} type="sales">
+          <Select column="name" />
+          <Select column="subsidiary" />
+          <Select column="number" />
+          <Select column="location" />
+        </Search>
+      </Field>
+      <Field label="Currency" type="number" column="currency" />
+    </Sublist>
+  </Tab>
+  <Button label="Clear Button" />
+</Form>; */
+}
